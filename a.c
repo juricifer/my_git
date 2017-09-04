bbd362556f6688c455c17eb0347be87e0d97d7f8
jgd <Tao.Fang@desay-svautomotive.com>
2017-09-04 12:01:14 +0800

    my c file

---
+++ a.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: a.c
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: Sat 19 Apr 2014 09:14:16 AM HKT
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("[%s: %3u]ssss\n", __FILE__, __LINE__);
+    printf("[%s: %3u]addddd\n", __FILE__, __LINE__);
+    return 0;
+}
+
